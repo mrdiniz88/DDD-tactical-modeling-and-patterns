c2dd4ae1dd63b3ae3ca71f2d96e3ba3329a5b6a0
mrdiniz88 <mr.diniz8888@gmail.com>
2022-08-31 18:02:37 -0300

    feat(domain): Adds the address value object in the client

---
+++ src/domain/customer/value-object/address.ts
@@ -0,0 +1,49 @@
+export default class Address {
+  private _street: string = "";
+  private _number: number = 0;
+  private _zip: string = "";
+  private _city: string = "";
+
+  constructor(street: string, number: number, zip: string, city: string) {
+    this._street = street;
+    this._number = number;
+    this._zip = zip;
+    this._city = city;
+
+    this.validate();
+  }
+
+  get street(): string {
+    return this._street;
+  }
+
+  get number(): number {
+    return this._number;
+  }
+
+  get zip(): string {
+    return this._zip;
+  }
+
+  get city(): string {
+    return this._city;
+  }
+
+  validate() {
+    if (this._street.length === 0) {
+      throw new Error("Street is required");
+    }
+
+    if (this._number == 0) {
+      throw new Error("Number is required");
+    }
+
+    if (this._zip.length === 0) {
+      throw new Error("Zip is required");
+    }
+
+    if (this._city.length === 0) {
+      throw new Error("City is required");
+    }
+  }
+}
